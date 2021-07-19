04fbeb60b1e4c307ef70375eb9a53479d4b55533
zing <599490911@qq.com>
2021-07-19 09:58:23 +0800

    update http xiandao

--- protocol/http/xiandao/xiandao.go
+++ protocol/http/xiandao/xiandao.go
@@ -2,17 +2,21 @@ package xiandao
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/zing-dev/atian-tools/log"
 	"github.com/zing-dev/atian-tools/protocol/common"
 	"io"
 	"net/http"
+	"time"
 )
 
 // Request 报警请求
 type Request struct {
 	LocationCode string `json:"locationCode"` //库位编码
+	Status       int    `json:"status"`       //1报警 99心跳
 }
 
 // Response 服务端响应
@@ -22,8 +26,22 @@ type Response struct {
 }
 
 type HTTP struct {
+	ctx    context.Context
+	cancel context.CancelFunc
 	URL    string
 	Client http.Client
+}
+
+func New(ctx context.Context, url string) *HTTP {
+	ctx, cancel := context.WithCancel(ctx)
+	h := &HTTP{
+		ctx:    ctx,
+		cancel: cancel,
+		URL:    url,
+		Client: http.Client{Timeout: time.Second * 2},
+	}
+	go h.ping()
+	return h
 }
 
 // Post 发送POST请求
@@ -48,4 +66,26 @@ func (h *HTTP) Post(request Request) (*Response, error) {
 	r := new(Response)
 	err = json.Unmarshal(data, r)
 	return r, err
+}
+
+func (h *HTTP) ping() {
+	for {
+		select {
+		case <-h.ctx.Done():
+			return
+		case <-time.After(time.Second * 30):
+			response, err := h.Post(Request{LocationCode: "", Status: 99})
+			if err != nil {
+				log.L.Error("心跳失败: ", err)
+				continue
+			}
+
+			if response.Code != 0 {
+				log.L.Error(fmt.Sprintf("心跳失败,返回数据: %s", response.Msg))
+				continue
+			}
+
+			log.L.Info("发送心跳成功")
+		}
+	}
 }
